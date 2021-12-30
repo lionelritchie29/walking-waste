aab56bf2c4c117893fa46b32853b8f6634fa3f24
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-12-30 14:22:08 +0700

    update browser width validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -9,9 +9,27 @@ function App() {
   const [user, setUser] = useContext(UserContext);
   const navigate = useNavigate();
 
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
   useEffect(() => {
-    if (window.innerWidth > 512) {
-      alert('This web app is suitable for mobile phone only');
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (dimensions.width > 512) {
+      alert(
+        'This web app is suitable for mobile phone only, if you are on a web browser, than you must open resize the browser width below 512px',
+      );
     }
   }, []);
 
@@ -26,6 +44,15 @@ function App() {
       navigate('/login');
     }
   };
+
+  if (dimensions.width > 512)
+    return (
+      <div className='flex flex-col justify-center items-center h-screen text-xs w-1/2 mx-auto'>
+        This web app can only be opened on screen less than 512px (mobile only).
+        Resize your browser if you are on a web browser and refresh the page!
+        <div className='mt-4'>Sorry for the inconvenience 😊</div>
+      </div>
+    );
 
   return (
     <main className='bg-gray-200 md:min-h-screen'>
